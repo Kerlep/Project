ff6d3d56284edea1ea4c1ca68a4c95da3cb061ce
Victoria Kerlep <89938975+Kerlep@users.noreply.github.com>
2024-01-31 09:07:40 +0300

    Add files via upload
    /бот попугай

---
+++ main.py
@@ -0,0 +1,23 @@
+import telebot
+
+bot = telebot.TeleBot('токен')
+
+# Обработчик текста
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+# Обработчик изображений
+@bot.message_handler(content_types=['photo'])
+def echo_photo(message):
+    bot.send_photo(message.chat.id, message.photo[-1].file_id)
+
+# Обработчик стикеров
+@bot.message_handler(content_types=['sticker'])
+def echo_sticker(message):
+    bot.send_sticker(message.chat.id, message.sticker[-1].file_id)
+
+# Запуск бота
+bot.polling()
+
